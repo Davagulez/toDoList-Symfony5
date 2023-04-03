917874bf264b1eb28b91c2d5929ba6a008fb4b4e
David Lezcano <lezcadav@gmail.com>
2023-04-03 13:48:17 -0300

    add navbar and styles

---
+++ public/js/header.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('#nav');
+const onScroll = () => {
+    const scrollPosition = window.scrollOffsetY;
+    nav
+     .classList
+     .toggle(
+        "scrolled-down",
+        scrollPosition > 56
+     );
+};
+
+document.addEventListener("scroll", onScroll, {passive:true});
